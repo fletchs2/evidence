0be2d9bc41af32cdbc15ff573dc2ae0abf751f26
Christopher Fonnesbeck <fonnescj@Honus.local>
2010-11-11 15:15:00 -0600

    Added first unit test

---
+++ test/test_likelihoods.R
@@ -0,0 +1,13 @@
+library(testthat)
+
+test_that("Poisson likelihood validation",
+    {
+        x <- rpois(5, 3)
+        a <- lpois(x)
+        pl <- plot(a)
+        b <- lpois(x, robust=T)
+        plot(b, pl)
+        
+        expect_that(a$x[a$lx==1], equals(mean(x)))
+    }
+)
