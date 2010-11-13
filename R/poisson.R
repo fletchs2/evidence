67b4773042fec7d23bc932f47ca6acc1aae873f8
Christopher Fonnesbeck <fonnescj@honus.gateway.2wire.net>
2010-11-13 17:15:06 -0600

    Added epois function.

--- R/poisson.R
+++ R/poisson.R
@@ -37,3 +37,67 @@ lpois <- function(y, lo=NA, hi=NA, robust=F, scale=T)
     likelihood$dist <- "Poisson"
     likelihood
 }
+
+# TODO Add probability of failing to find strong evidence to Poisson.
+
+#' Probability of misleading evidence for Poisson distribution
+#' 
+#' L(p)/L(trueprob) >= k
+#' 
+#' @param n Binomial sample size.
+#' @param truemean True mean.
+#' @param truevar True variance for normal approximation only (optional).
+#' @param lo Lower parameter bound to likelihood calculation (optional).
+#' @param hi Upper parameter bound to likelihood calculation (optional).
+#' @param robust Flag for calculating robust correction (defaults to FALSE)
+#' @param p.fail Flag for calculating probability of failing to find strong  
+#'      evidence supporting trueprob (defaults to FALSE).
+epois <- function(n, truemean, truvar=F, r=NA, lo=NA, hi=NA, k=8, robust=F, approx=F, p.fail=F) {
+    
+    # Set default bounds if none provided
+    if(is.na(lo)) lo <- max(truemean - 10 * sqrt(truemean/n), 0.001)
+    if(is.na(hi)) hi <- truemean + 10 * sqrt(truemean/n)
+    
+    # Less than true mean
+    s.lo <- (truemean - lo)/500
+    mean.lo <- seq(lo, truemean - s.lo, s.lo)
+    crit.lo <- pmax((log(k) + n*(mean.lo - truemean))/log(mean.lo/truemean),  0)
+
+    # Greather than true mean
+    s.hi <- (truemean - hi)/500
+    mean.hi <- seq(hi, truemean-s.hi, s.hi)
+    crit.hi <- (log(k) + n*(mean.hi - truemean))/log(mean.hi/truemean)
+    
+    mean <- c(mean.lo, truemean, mean.hi)
+    
+    # Normal approximation
+    if (approx==T) {
+        mislead <- 1 - pnorm(((sqrt(n) * abs(mean - truemean))/2 + (truemean * log(8))/(sqrt(n) * abs(mean - truemean)))/sqrt(truevar))
+    }
+    # Negative binomial model
+    else if (!is.na(r)) {
+        
+        # a/b= negbin mean / negbin var = p
+        if (robust==T) {
+            k1 <- k^(1/(r/(r + truemean)))
+			mislead.lo <- pnbinom(floor((n * (mean.lo - truemean) + log(k1))/log(mean.lo/truemean)), n * r, r/(r + truemean))
+			mislead.hi <- 1 - pnbinom(floor((n * (mean.hi - truemean) + log(k1))/log(mean.hi/truemean)), n * r, r/(r + truemean))
+    		mislead <- c(mislead.lo, 0, mislead.hi)
+        }
+        else {
+            mislead.lo <- pnbinom(floor((n * (mean.lo - truemean) + log(k))/log(mean.lo/truemean)), n * r, r/(r + truemean))
+            mislead.hi <- 1 - pnbinom(floor((n * (mean.hi - truemean) + log(k))/log(mean.hi/truemean)), n * r, r/(r + truemean))
+    		mislead <- c(mislead.lo, 0, mislead.hi)
+        }
+    }
+    else {
+        mislead.lo <- ppois(floor(crit.lo), n*truemean)
+        mislead.hi <- 1 - ppois(ceiling(crit.hi) - 1, n*truemean)
+        mislead <- c(mislead.lo, 0, mislead.hi)
+        
+    }
+    
+    error <- list(mislead=list(x=mean, px=mislead), dist="Poisson")
+    class(error) <- "error"
+    error
+}
