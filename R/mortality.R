fd907012a5c692f3f3ebd07205f4ca42b2ca094c
Chris Fonnesbeck <chris.fonnesbeck@vanderbilt.edu>
2011-04-15 09:44:04 -0500

    Added likelihoods for mortality ratios; changed notation in odds ratio likelihood.

--- R/mortality.R
+++ R/mortality.R
@@ -1,1 +1,73 @@
-# TODO Add likelihoods for standardized mortality ratio
+#' Standardized mortality ratio likelihood
+#'
+#' Calculates likelihood function for expected value
+#' of the standardized mortality ratio (SMR) 
+#' i.e., E(observed deaths/expected deaths),  under Poisson
+#' model for observed deaths, taking expected deaths to be fixed.
+#' 
+#' @param obs_deaths Observed number of deaths.
+#' @param exp_deaths Expected number of deaths.
+#' @param lo Lower parameter bound to likelihood calculation (defaults to 0.1).
+#' @param hi Upper parameter bound to likelihood calculation (defaults to 5).
+#' @param points Number of points in [0,1] to calculate likelihood 
+#'      (defaults to 1000).
+#' @param scale Flag for scaling maximum likelihood to 1 (defaults to TRUE).
+#' @return likelihood object.
+#' @keywords likelihood mortality ratio poisson
+#' @export
+odds_like <- function(obs_deaths, exp_deaths, lo=0.1, hi=5, points=1000, scale=T) {
+    
+    # Range of ratio
+    z <- seq(lo, hi, length=points)
+    
+    # Calculate likelihood
+	like <- obs_deaths * log(z*exp_deaths) - z*exp_deaths - lgamma(obs_deaths + 1)
+    
+    if (scale==T) {
+        like <- like - max(like)
+    }
+
+    # Instantiate likelihood object
+    likelihood <- list(x=z, lx=exp(like))
+    class(likelihood) <- "likelihood" 
+    likelihood
+}
+
+
+#' Standardized ratio of mortality rates likelihood
+#'
+#' Calculates likelihood function for ratio of
+#' SMR from two causes. 
+#' i.e., E(d1/e1)/E(d2/e2)= [E(d1)/E(d2)]/[e1/e2], under independent
+#' Poisson model for d1 and d2.
+#' 
+#' @param obs1 Observed number of deaths, cause 1.
+#' @param exp1 Expected number of deaths, cause 1.
+#' @param obs2 Observed number of deaths, cause 2.
+#' @param exp2 Expected number of deaths, cause 2.
+#' @param lo Lower parameter bound to likelihood calculation (defaults to 0.1).
+#' @param hi Upper parameter bound to likelihood calculation (defaults to 5).
+#' @param points Number of points in [0,1] to calculate likelihood 
+#'      (defaults to 1000).
+#' @param scale Flag for scaling maximum likelihood to 1 (defaults to TRUE).
+#' @return likelihood object.
+#' @keywords likelihood mortality ratio poisson
+#' @export
+odds_like <- function(obs1, exp1, obs2, exp2, lo=0.1, hi=5, points=1000, scale=T) {
+    
+    # Range of ratio
+    z <- seq(lo, hi, length=points)
+    
+    # Calculate likelihood
+    c1 <- exp2/exp1
+	like <- obs1 * log(z/(z + c1)) + obs2 * log(c1/(z + c1))
+    
+    if (scale==T) {
+        like <- like - max(like)
+    }
+
+    # Instantiate likelihood object
+    likelihood <- list(x=z, lx=exp(like))
+    class(likelihood) <- "likelihood" 
+    likelihood
+}
--- R/odds.R
+++ R/odds.R
@@ -3,26 +3,26 @@
 #' For binomial counts x and y, with sample sizes m and n, respectively,
 #' calculates conditional likelihood.
 #' 
-#' @param x Binomial count, sample 1.
-#' @param m Binomial sample size, sample 1.
-#' @param y Binomial count, sample 2.
-#' @param n Binomial sample size, sample 2.
+#' @param y1 Binomial count, sample 1.
+#' @param n1 Binomial sample size, sample 1.
+#' @param y2 Binomial count, sample 2.
+#' @param n2 Binomial sample size, sample 2.
 #' @param lo Lower parameter bound to likelihood calculation (defaults to 0).
 #' @param hi Upper parameter bound to likelihood calculation (defaults to 1).
 #' @param points Number of points in [0,1] to calculate likelihood 
 #'      (defaults to 1000).
 #' @param scale Flag for scaling maximum likelihood to 1 (defaults to TRUE).
 #' @return likelihood object.
-#' @keywords likelihood
+#' @keywords likelihood odds ratio binomial
 #' @export
-odds_like <- function(x, m, y, n, lo=NA, hi=NA, points=1000, scale=T) {
+odds_like <- function(y1, n1, y2, n2, lo=NA, hi=NA, points=1000, scale=T) {
     
-    k <- x + y
-    s <- max(k - n, 0):min(m, k)
-    lcoeff <- lgamma(x + 1) + lgamma(m - x + 1) + lgamma(k - x + 1) + lgamma(n - k + x + 1) - lgamma(s + 1) - lgamma(m - s + 1) - lgamma(k - s + 1) - lgamma(n - k + s + 1)
+    k <- y1 + y2
+    s <- max(k - n2, 0):min(n1, k)
+    lcoeff <- lgamma(y1 + 1) + lgamma(n1 - y1 + 1) + lgamma(k - y1 + 1) + lgamma(n2 - k + y1 + 1) - lgamma(s + 1) - lgamma(n1 - s + 1) - lgamma(k - s + 1) - lgamma(n2 - k + s + 1)
         
-    psi <- (x * (n - y))/(y * (m - x))  
-    std <- sqrt(1/x + 1/y + 1/(m - x) + 1/(n - y))
+    psi <- (y1 * (n2 - y2))/(y2 * (n1 - y1))  
+    std <- sqrt(1/y1 + 1/y2 + 1/(n1 - y1) + 1/(n2 - y2))
 	ll <- exp(log(psi) - 3 * std)
 	ul <- exp(log(psi) + 2.7 * std)
     
@@ -33,7 +33,7 @@ odds_like <- function(x, m, y, n, lo=NA, hi=NA, points=1000, scale=T) {
     
     Tz <- matrix(z, nrow = length(z), ncol = length(
 		s))
-	Tz <- (t(Tz))^(s - x)
+	Tz <- (t(Tz))^(s - y1)
 	like <- 1/(t(Tz)) %*% exp(lcoeff)
     
     psihat <- z[like==max(like)]
@@ -45,7 +45,7 @@ odds_like <- function(x, m, y, n, lo=NA, hi=NA, points=1000, scale=T) {
     }
 
     # Instantiate likelihood object
-    likelihood <- list(x=z, lx=like)
+    likelihood <- list(x=z, lx=like, LR=LR)
     class(likelihood) <- "likelihood" 
     likelihood
 }
